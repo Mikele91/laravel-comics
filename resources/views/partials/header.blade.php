527c3705470e3725b57efec3dd0eb45d33cc2b7a
Mikele91 <michele.amoruso91.8@gmail.com>
2021-11-05 18:02:06 +0100

    errore background

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,40 @@
-<h1>header prova</h1>
+<header>
+
+        <nav class="nav_top">
+            <div class="container">
+                <span> 
+                    DC POWER &#8480 VISA &#174
+                    <span class="trianlge">&#9660</span>
+                </span>
+            </div>
+     
+        </nav>
+
+        <nav class="nav_bottom">
+            <div class="container">
+               <div class="logo">
+                   <img src="{{asset("images/dc-logo.png")}}" alt="">
+               </div>
+               <ul>
+                   <li><a href="#">CHARACTERS</a></li>
+                   <li><a class="active" href="#">COMICS</a></li>
+                   <li><a href="#">MOVIES</a></li>
+                   <li><a href="#">TV</a></li>
+                   <li><a href="#">GAMES</a></li>
+                   <li><a href="#">COLLECTIBLES</a></li>
+                   <li><a href="#">VIDEOS</a></li>
+                   <li><a href="#">FANS</a></li>
+                   <li><a href="#">NEWS</a></li>
+                   <li><a href="#">SHOP<span class="trianlge">&#9660</span></a></li>
+               </ul>
+               <div class="search">
+                   <p>search 🔍</p>
+
+               </div>
+            </div>
+        </nav>
+
+    <section class="hero">
+
+    </section>
+</header>
